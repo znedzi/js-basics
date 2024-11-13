c3b14db8339a49e8157858a7cd0f8589fc88e4f5
Zbigniew Nędzi <znedzi@wp.pl>
2024-11-13 22:09:51 +0100

    Start. Data typej in JS

---
+++ task-0/js/script.js
@@ -0,0 +1,27 @@
+// PRIMITIVES
+
+// Number
+5
+1.5
+// String
+'Hello'
+"J can't"
+// Boolean
+true
+false
+// null
+null
+// undefined
+undefined
+
+// OBJECTS
+
+// Object
+{
+    name: 'Drakula',
+    age: 7
+}
+
+// Array - (subtime or kind of object) - tablica jest rodzajem obiektu
+['Drakula', 7]
+// Functions are also Objects !!!
